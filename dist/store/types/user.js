def405788c618fc005b642c5228fda68afc1b046
Is-Yang <1255786936@qq.com>
2020-03-13 18:02:08 +0800

    feat

---
+++ dist/store/types/user.js
@@ -0,0 +1,7 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var UPDATE_GLOBAL_DATA = exports.UPDATE_GLOBAL_DATA = 'UPDATE_GLOBAL_DATA';
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInVzZXIuanMiXSwibmFtZXMiOlsiVVBEQVRFX0dMT0JBTF9EQVRBIl0sIm1hcHBpbmdzIjoiOzs7OztBQUFPLElBQU1BLGtEQUFxQixvQkFBM0IiLCJmaWxlIjoidXNlci5qcyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBVUERBVEVfR0xPQkFMX0RBVEEgPSAnVVBEQVRFX0dMT0JBTF9EQVRBJzsiXX0=
