84a8bd509bf439a05556349211866c3c0e98446e
is-yang <1255786936@qq.com>
2020-03-23 22:44:06 +0800

    feat 接口联调

--- dist/npm/redux-actions/lib/utils/flattenActionMap.js
+++ dist/npm/redux-actions/lib/utils/flattenActionMap.js
@@ -1,14 +1,1 @@
-"use strict";
-
-exports.__esModule = true;
-exports.default = void 0;
-
-var _isPlainObject = _interopRequireDefault(require('./isPlainObject.js'));
-
-var _flattenWhenNode = _interopRequireDefault(require('./flattenWhenNode.js'));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var _default = (0, _flattenWhenNode.default)(_isPlainObject.default);
-
-exports.default = _default;
+"use strict";function _interopRequireDefault(e){return e&&e.__esModule?e:{default:e}}exports.__esModule=!0,exports.default=void 0;var _isPlainObject=_interopRequireDefault(require("./isPlainObject.js")),_flattenWhenNode=_interopRequireDefault(require("./flattenWhenNode.js")),_default=(0,_flattenWhenNode.default)(_isPlainObject.default);exports.default=_default;
